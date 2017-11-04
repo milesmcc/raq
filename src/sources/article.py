95862c5710aeb2eae5e2e618ff84aec1b3fffda7
Darcy Meyer <darcy2252755@gmail.com>
2017-11-04 12:48:59 -0400

    update article class

--- src/sources/article.py
+++ src/sources/article.py
@@ -1,7 +1,11 @@
 class Article:
-    def __init__(self, content):
-    	self.content = content
+    def __init__(self, url):
+    	self.url = url
+    	scrape()
 
     def getText(self):
     	return self.content
+
+    def scrape(self):
+    	pass
 
