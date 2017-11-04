aed743ef8fa8856ec24d2b2a22080504da68d48c
Miles McCain <milesmcc@users.noreply.github.com>
2017-11-04 12:26:00 -0400

    .

---
+++ src/topic.py
@@ -0,0 +1,22 @@
+from topicrawdata import TopicRawData
+
+from elements.sentiment import Sentiment
+
+class Topic:
+    known_elements = [Sentiment(),]
+
+    def __init__(self, string_query):
+        self.string_query
+        self.elements = {}
+
+    def assemble(self):
+        # pull data from online
+        topicrawdata = TopicRawData(self.string_query)
+        topicrawdata.populate()
+
+        # perform analysis
+        for element in known_elements:
+            self.elements[element.get_name()] = element.process(topicrawdata)
+
+        # find related topics
+        self.rela
