605df5b5e0b2baadcc8f777130d890b342fe447a
Miles McCain <milesmcc@users.noreply.github.com>
2017-11-04 11:56:20 -0400

    add package structure

---
+++ src/related_topics.py
@@ -0,0 +1,14 @@
+class RelatedTopics:
+    __init__(self):
+        pass
+
+    def get_name():
+        return "Related Topics"
+
+    """
+    strings: an array of strings on which to do the data analysis
+    """
+    def process(self, strings):
+
+        # return your list of related topic strings here
+        return {}
