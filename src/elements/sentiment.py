fef43c8512544821256b6d00b91425bdeb686225
Miles McCain <milesmcc@users.noreply.github.com>
2017-11-04 12:07:35 -0400

    create boilerplate template

--- src/elements/sentiment.py
+++ src/elements/sentiment.py
@@ -1,14 +1,18 @@
-class RelatedTopics:
+class Sentiment:
     __init__(self):
         pass
 
     def get_name():
-        return "Related Topics"
+        return "Sentiment"
 
     """
     strings: an array of strings on which to do the data analysis
     """
     def process(self, topicdata):
 
+        strings = topicdata.get_strings()
+
         # return your list of related topic strings here
-        return {}
+        return {
+            "sentiment": -0.3
+        }
