26863fdcc7c0a4f3e1795b033a8b0c7e5b3a6719
localhost <localhost@localhosts-MacBook-Pro.local>
2017-11-04 16:26:52 -0400

    test

--- raq/topicrawdata.py
+++ raq/topicrawdata.py
@@ -1,5 +1,5 @@
 import requests
-from .sources.article import Article
+from sources.article import Article
 import os
 
 secrets = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../secrets.txt")
