c9137838964d4a3808d2e71ceab8e2036dad3098
Darcy Meyer <darcy2252755@gmail.com>
2017-11-04 15:37:46 -0400

    intermediate push

--- src/sources/article.py
+++ src/sources/article.py
@@ -1,14 +1,12 @@
-# import newspaper
+import newspaper
 
 class Article:
     def __init__(self, url):
     	self.url = url
-    	# self.scrape()
+    	self.scrape()
 
     def get_text(self):
-        #temp
-        return '''heres some dummy article text'''
-    	# return self.article.text
+    	return self.article.text
 
     def get_headline(self):
         return self.article.title
