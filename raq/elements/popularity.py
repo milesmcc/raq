559704a21b6a3a525dfc18938f3c3c6f7446480c
Miles McCain <milesmcc@users.noreply.github.com>
2017-11-04 16:23:31 -0400

    implement popularity

--- raq/elements/popularity.py
+++ raq/elements/popularity.py
@@ -8,22 +8,17 @@ import requests
 import unirest
 from topia.termextract import extract
 
-class RelatedArticles:
+class Popularity:
     def __init__(self):
         pass
 
     def get_name(self):
-        return "RelatedArticles"
+        return "Popularity"
 
     def get_human_readable_name(self):
-        return "Related Articles"
+        return "Popularity (Mentions)"
 
     def process(self, topicrawdata):
-        toparticles = topicrawdata.articles[:5]
         return {
-            "top_articles": [{
-                "headline": article.get_headline(),
-                "thumbnail": article.get_thumbnail(),
-                "url": article.get_url()
-            } for article in toparticles]
+            "popularity": topicrawdata.search_results
         }
--- raq/topic.py
+++ raq/topic.py
@@ -1,6 +1,9 @@
 from .topicrawdata import TopicRawData
 from .elements.related_topics import RelatedTopics
 from .elements.sentiment import Sentiment
+from .elements.popularity import Popularity
+from .elements.related_articles import RelatedArticles
+
 
 class Topic:
     '''return dict of relevant info about a single topic'''
@@ -8,7 +11,7 @@ class Topic:
     def __init__(self, string_query):
         self.string_query = string_query
         self.elements = {}
-        self.known_analyses = [Sentiment(), RelatedTopics()]
+        self.known_analyses = [Sentiment(), RelatedTopics(), Popularity(), RelatedArticles()]
         self.assemble()
 
     def get_related_topics(self):
