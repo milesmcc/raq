f6c1a4f9a9e5e0017a26327a7b172237e85f6592
Miles McCain <milesmcc@users.noreply.github.com>
2017-11-04 13:26:14 -0400

    remove tweets, wikipedia, reddit

--- src/topicrawdata.py
+++ src/topicrawdata.py
@@ -3,10 +3,7 @@ class TopicRawData:
     def __init__(self, topicstring):
         self.topicstring = topicstring
 
-        self.tweets = None # array of tweets, according to Twitter's specification
         self.articles = None # array of article classes
-        self.wikipedia = None # array of Wikipedia classes
-        self.reddit = None # array of Reddit comments
 
     def populate(self):
         pass
