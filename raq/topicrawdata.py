5ee7064159754a0069bb89d1afd2fb1e17f9c2a4
localhost <localhost@localhosts-MacBook-Pro.local>
2017-11-04 17:42:14 -0400

    working

--- raq/topicrawdata.py
+++ raq/topicrawdata.py
@@ -3,9 +3,17 @@ from sources.article import Article
 import os
 import json
 import multiprocessing as mp
-
+import pdb
 
 secrets = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../secrets.txt")
+
+def go(self, article):
+    try:
+        articleObj = Article(article["url"], article["name"])
+        articleObj.thumbnail = article["image"]["thumbnail"]["contentUrl"]
+        return articleObj
+    except:
+        print("ERROR ON " + article["url"])
 
 class TopicRawData:
     '''get the raw data from a lot of sources and put it into a '''
@@ -16,25 +24,19 @@ class TopicRawData:
 
         self.subscriptionKey = secret = open(secrets).readlines()[3]
 
-    def go(self, article):
-        try:
-            articleObj = Article(article["url"], article["name"])
-            articleObj.thumbnail = article["image"]["thumbnail"]["contentUrl"]
-            self.articles.append(articleObj)
-        except:
-            print("ERROR ON " + article["url"])
-
     def populate(self):
+        p = mp.Pool(20)
         host = "https://api.cognitive.microsoft.com/bing/v7.0/news"
         headers = {'Ocp-Apim-Subscription-Key': self.subscriptionKey.strip()}
         response = requests.get(host + "?q=" + self.topicstring + "&searchFilters=News", headers=headers).json()
-        p = mp.Pool(10)
-        results = []
+        results=[]
         for i in response["value"][:4]:
-            results.append(p.apply_async(self.go, args=(self, i, )))
+            results.append(p.apply_async(go, args=(self, i, )))
         p.close()
         p.join()
-
+        results = [x.get() for x in results]
+        results = filter(None, results)
+        self.articles.extend(results)
         print json.dumps(response)
         self.search_results = len(response["value"])
 
